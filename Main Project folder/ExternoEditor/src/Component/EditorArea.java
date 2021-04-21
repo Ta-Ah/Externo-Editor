d1312c460ef9e53258ba137a4ec51e211b016e63
Tamal <69917079+Ta-Ah@users.noreply.github.com>
2021-04-21 16:52:30 +0600

    Add files via upload

---
+++ Main Project folder/ExternoEditor/src/Component/EditorArea.java
@@ -0,0 +1,14 @@
+package Component;
+
+import org.fxmisc.richtext.CodeArea;
+import org.fxmisc.richtext.LineNumberFactory;
+
+
+public class EditorArea extends CodeArea {
+
+    public EditorArea() {
+        this.setWrapText(true);
+        this.setParagraphGraphicFactory(LineNumberFactory.get(this));
+    }
+}
+
